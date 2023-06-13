ff7f3e9bbdb6e06c0bbbfd34faabaff127152f76
Liam Sho <conan_creeper@outlook.com>
2023-06-13 19:47:03 +0800

    feat: add placeholder api and format welcome message

--- src/main/java/moe/alisalab/nibiruconnector/handlers/PlayerJoinEventHandler.java
+++ src/main/java/moe/alisalab/nibiruconnector/handlers/PlayerJoinEventHandler.java
@@ -1,12 +1,16 @@
 package moe.alisalab.nibiruconnector.handlers;
 
+import eu.pb4.placeholders.api.PlaceholderContext;
+import eu.pb4.placeholders.api.Placeholders;
+import eu.pb4.placeholders.api.TextParserUtils;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 
 public final class PlayerJoinEventHandler {
 
     public static void sendPlayerMessageAfterJoin(ServerPlayerEntity player) {
-        player.sendMessage(Text.literal("Hi!"));
+        var message = Placeholders.parseText(
+                TextParserUtils.formatText("> Welcome to <gr:#f959ff:#5be3e9><bold>Nibiru SMP</bold></gr>, %player:displayname%!"),
+                PlaceholderContext.of(player));
+        player.sendMessage(message);
     }
-
 }
